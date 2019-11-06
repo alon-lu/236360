d62291d297cad95d1b325939e217896d5692dec1
mon <davidbr141@gmail.com>
2019-11-06 23:53:32 +0200

    added error handling

--- hw1/part_a.cpp
+++ hw1/part_a.cpp
@@ -21,58 +21,75 @@ char toInsert(char next) {
             return '\\';
         case '"':
             return '"';
-				case 'x':
-		        return 'x';
+        case 'x':
+            return 'x';
+        default:
+            return 0;
     }
 }
 
-char fromAscii(char i,char j){
-  if((i>='0' && i<='7')){
-        int first= i-'0';
-        int second=0;
-        if(j>='0' && j<='9'){
-          second= j-'0';
+char fromAscii(char i,char j) {
+    if ((i >= '0' && i <= '7')) {
+        int first = i - '0';
+        int second = 0;
+        if (j >= '0' && j <= '9') {
+            second = j - '0';
         }
-        if ((j>='a' && j<='f')) {
-          second= j-'a'  + 1 ;
+        if ((j >= 'a' && j <= 'f')) {
+            second = j - 'a' + 1;
         }
-				if ((j>='A' && j<='F')) {
-					second= j-'A' + 1 ;
-				}
-				return first*10 + second;
-      }
+        if ((j >= 'A' && j <= 'F')) {
+            second = j - 'A' + 1;
+        }
+        return first * 10 + second;
+    }
 }
 
 void showToken(const int token) {
+    if (token == WRONGCHAR) {
+
+        printf("Error <char>\n", yytext);
+        exit(0);
+    }
     if (token == COMMENT) {
         printf("%d %s %s\n", yylineno, FRUIT_STRING[token], "//");
-    } else
-    if (token == STRING) {
-        int lines=0;
+    } else if (token == STRING) {
         int strlen = stringLen(yytext);
         std::string outputString;
-        for (int i = 1; i < strlen -1 ; i++) {
+        for (int i = 1; i < strlen - 1; i++) {
             if (yytext[i] != '\\')
                 outputString.push_back(yytext[i]);
             else {
-								char insert = toInsert(yytext[i + 1]);
-								if( insert == 'x' ){
-									insert = fromAscii(yytext[i + 2],yytext[i + 3]);
-								}
+                char insert = toInsert(yytext[i + 1]);
+                if (insert == 'n') {
+                    printf("Error enclosed string\n");
+                    exit(0);
+                }
+                if (insert == 0) {
+                    printf("Error undefined escape sequence q\n");
+                    exit(0);
+                }
+                if (insert == 'x') {
+                    insert = fromAscii(yytext[i + 2], yytext[i + 3]);
+                }
                 outputString.push_back(insert);
                 i++;
             }
         }
+        if (*outputString.end()=='\\'){
+            printf("Error unclosed string\n");
+            exit(0);
+        }
         printf("%d %s ", yylineno, FRUIT_STRING[token]);
-        std::cout<<outputString<<std::endl;
+        std::cout << outputString << std::endl;
     } else {
         printf("%d %s %s\n", yylineno, FRUIT_STRING[token], yytext);
     }
 }
 
-int main(){
+int main() {
     int token;
-    while(token = yylex()) {
+    while (token = yylex()) {
         showToken(token);
     }
     return 0;
